ae159649da945d2dc73917607410a23ef7e46179
Leigh Pauls <leighpauls@fb.com>
2016-01-18 19:21:52 -0800

    [logger] Send some test log messages

--- src/org/usfirst/frc/team254/robot/TestingMain.java
+++ src/org/usfirst/frc/team254/robot/TestingMain.java
@@ -10,5 +10,14 @@ public class TestingMain {
         cheesyLogger.sendLogMessage("hello");
         cheesyLogger.sendTimePlotPoint("pi", 3.14);
         cheesyLogger.sendScatterPlotPoint("position", 23.2323232323, 42);
+
+        while (true) {
+            cheesyLogger.sendTimePlotPoint("test_value", System.currentTimeMillis() % 10000);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
