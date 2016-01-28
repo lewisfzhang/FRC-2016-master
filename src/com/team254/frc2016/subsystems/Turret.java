13a046393cce1d92f14f39dbbe55742732c972c9
Tom Bottiglieri <tom.bottiglieri@gmail.com>
2016-01-27 18:35:29 -0800

    Add Turret subsytem
    This will hold the Talon for the turret and implement some
    high level angle set/get methods.

---
+++ src/com/team254/frc2016/subsystems/Turret.java
@@ -0,0 +1,23 @@
+package com.team254.frc2016.subsystems;
+
+import com.team254.frc2016.Constants;
+import edu.wpi.first.wpilibj.CANTalon;
+
+public class Turret {
+    private static Turret ourInstance = new Turret();
+
+    public static Turret getInstance() {
+        return ourInstance;
+    }
+
+    private CANTalon talon;
+
+    private Turret() {
+        talon = new CANTalon(Constants.kTurretTalonId);
+        talon.changeControlMode(CANTalon.TalonControlMode.Position);
+    }
+
+    public void setAngle(double angle) {
+
+    }
+}
