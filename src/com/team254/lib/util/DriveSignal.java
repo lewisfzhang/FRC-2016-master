d92f61a797f7a826186c820d9550a7b451a1edd2
Ryan Johnson <ryan@johnsonweb.us>
2016-01-30 16:09:33 -0800

    Cheesy Drive Cleaned

---
+++ src/com/team254/lib/util/DriveSignal.java
@@ -0,0 +1,20 @@
+package com.team254.lib.util;
+
+/**
+ * Created by ryanjohnson on 1/30/16.
+ */
+public class DriveSignal {
+    public double leftMotor;
+    public double rightMotor;
+
+    public DriveSignal(double left, double right) {
+        this.leftMotor = left;
+        this.rightMotor = right;
+    }
+
+    public static DriveSignal NEUTRAL = new DriveSignal(0, 0);
+
+    public String toString() {
+        return "L: " + leftMotor +  ", R: " + rightMotor;
+    }
+}
