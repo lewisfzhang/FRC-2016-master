43d38d5346fcb1087ee74a4cb2f66c29daa7805f
Jared Russell <jared@team341.com>
2016-02-02 17:36:48 -0800

    Add robot state for real

---
+++ src/com/team254/frc2016/RobotState.java
@@ -0,0 +1,117 @@
+package com.team254.frc2016;
+
+import java.util.Map;
+
+import com.team254.lib.util.InterpolatingTreeMap;
+import com.team254.lib.util.Pose2d;
+import com.team254.lib.util.Rotation2d;
+import com.team254.lib.util.Translation2d;
+
+/**
+ * RobotState keeps track of the poses of various inertial frames throughout the
+ * match.
+ * 
+ * Robot frames of interest (from parent to child):
+ * 
+ * 1. Odometric frame: origin is where the robot is turned on
+ * 
+ * 2. Vehicle frame: origin is the center of the robot wheelbase, facing
+ * forwards
+ * 
+ * 3. Turret fixed frame: origin is the center of the turret when the turret is
+ * at 0 degrees rotation
+ * 
+ * 4. Turret rotating frame: origin is the center of the turret as it rotates
+ * 
+ * 5. Camera frame: origin is the center of the camera imager as it rotates with
+ * the camera
+ * 
+ * As a simple kinematic chain with 5 frames, there are 4 transforms of
+ * interest:
+ * 
+ * 1. Odometric-to-vehicle: This is tracked over time by integrating encoder and
+ * gyro measurements. It will inevitably drift, but is usually accurate over
+ * short time periods.
+ * 
+ * 2. Vehicle-to-turret-fixed: This is a constant.
+ * 
+ * 3. Vehicle-to-turret-rotating: This is a pure rotation, and is tracked over
+ * time.
+ * 
+ * 4. Turret-rotating-to-camera: This is a constant.
+ * 
+ * @author Jared
+ */
+public class RobotState {
+    public static final int kObservationBufferSize = 100;
+
+    public static final Pose2d kVehicleToTurretFixed = new Pose2d(
+            new Translation2d(Constants.kTurretXOffset, Constants.kTurretYOffset),
+            Rotation2d.fromDegrees(Constants.kTurretAngleOffsetDegrees));
+
+    public static final Pose2d kTurretRotatingToCamera = new Pose2d(
+            new Translation2d(Constants.kCameraXOffset, Constants.kCameraYOffset),
+            Rotation2d.fromDegrees(Constants.kCameraAngleOffsetDegrees));
+
+    // nanoTime -> Pose2d or Rotation2d
+    protected InterpolatingTreeMap<Long, Pose2d> odometric_to_vehicle_;
+    protected InterpolatingTreeMap<Long, Rotation2d> turret_rotation_;
+
+    public RobotState(Pose2d initial_odometric_to_vehicle, Rotation2d initial_turret_fixed_to_turret_rotating) {
+        long now = System.nanoTime();
+        odometric_to_vehicle_ = new InterpolatingTreeMap<Long, Pose2d>(kObservationBufferSize);
+        odometric_to_vehicle_.put(now, initial_odometric_to_vehicle);
+        turret_rotation_ = new InterpolatingTreeMap<Long, Rotation2d>(kObservationBufferSize);
+        turret_rotation_.put(now, initial_turret_fixed_to_turret_rotating);
+    }
+
+    public synchronized Pose2d getOdometricToVehicle(long timestamp) {
+        return odometric_to_vehicle_.getInterpolated(timestamp);
+    }
+
+    public synchronized Map.Entry<Long, Pose2d> getLatestOdometricToVehicle() {
+        return odometric_to_vehicle_.lastEntry();
+    }
+
+    public synchronized Rotation2d getTurretRotation(long timestamp) {
+        return turret_rotation_.getInterpolated(timestamp);
+    }
+
+    public synchronized Map.Entry<Long, Rotation2d> getLatestTurretRotation() {
+        return turret_rotation_.lastEntry();
+    }
+
+    public synchronized Pose2d getOdometricToTurretRotated(long timestamp) {
+        return odometric_to_vehicle_.getInterpolated(timestamp).transformBy(kVehicleToTurretFixed)
+                .transformBy(Pose2d.fromRotation(turret_rotation_.getInterpolated(timestamp)));
+    }
+
+    public synchronized Pose2d getOdometricToCamera(long timestamp) {
+        return getOdometricToTurretRotated(timestamp).transformBy(kTurretRotatingToCamera);
+    }
+
+    public synchronized void addOdometricToVehicleObservation(long timestamp, Pose2d observation) {
+        odometric_to_vehicle_.put(timestamp, observation);
+    }
+
+    public synchronized void addTurretRotationObservation(long timestamp, Rotation2d observation) {
+        turret_rotation_.put(timestamp, observation);
+    }
+
+    public synchronized void addObservations(long timestamp, Pose2d odometric_to_vehicle, Rotation2d turret_rotation) {
+        addOdometricToVehicleObservation(timestamp, odometric_to_vehicle);
+        addTurretRotationObservation(timestamp, turret_rotation);
+    }
+
+    public Pose2d generateOdometryFromSensors(double left_encoder_delta_distance, double right_encoder_delta_distance,
+            Rotation2d current_gyro_angle) {
+        Pose2d last_measurement = getLatestOdometricToVehicle().getValue();
+        // Use average encoder distance for translation, and average gyro
+        // reading from previous to current measurement for rotation.
+        Pose2d differential_pose = new Pose2d(
+                new Translation2d((left_encoder_delta_distance + right_encoder_delta_distance) / 2, 0),
+                Rotation2d.fromRadians(
+                        (last_measurement.getRotation().inverse().rotateBy(current_gyro_angle)).getRadians() / 2));
+        return last_measurement.transformBy(differential_pose);
+    }
+}
