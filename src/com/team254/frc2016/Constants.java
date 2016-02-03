965b5941bf98e98ac5b89a8fcef7b4e8bb72fc52
Jared Russell <jared@team341.com>
2016-02-02 17:36:04 -0800

    Add RobotState

--- src/com/team254/frc2016/Constants.java
+++ src/com/team254/frc2016/Constants.java
@@ -3,6 +3,15 @@ package com.team254.frc2016;
 import com.team254.lib.util.ConstantsBase;
 
 public class Constants extends ConstantsBase {
+    // Pose of the turret frame w.r.t. the vehicle frame
+    public static double kTurretXOffset = -10.0;
+    public static double kTurretYOffset = 0.0;
+    public static double kTurretAngleOffsetDegrees = 0.0;
+    
+    // Pose of the camera frame w.r.t. the turret frame
+    public static double kCameraXOffset = -5.0;
+    public static double kCameraYOffset = 0.0;
+    public static double kCameraAngleOffsetDegrees = 0.0;
 
     public static int kVisionUDPPort = 5254;
 
