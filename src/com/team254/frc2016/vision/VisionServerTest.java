4984ab6b4f25eec9149df6e1ff55912f08bd7a98
Leigh Pauls <leighpauls@fb.com>
2016-02-13 22:06:03 -0800

    test logging

--- src/com/team254/frc2016/vision/VisionServerTest.java
+++ src/com/team254/frc2016/vision/VisionServerTest.java
@@ -4,6 +4,7 @@ public class VisionServerTest {
     public static class TestReceiver implements VisionUpdateReceiver {
         @Override
         public void gotUpdate(VisionUpdate update) {
+            System.out.println("num targets: " + update.getTargets().size());
             for (int i = 0; i < update.getTargets().size(); i++) {
                 TargetInfo target = update.getTargets().get(i);
                 System.out.println("Target: " + target.getAngle() + ", " + target.getDistance());
