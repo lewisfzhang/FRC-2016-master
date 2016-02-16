bf0fd4085f5b9ff85cb5a95a71bdae7875c8cdd7
Leigh Pauls <leighpauls@fb.com>
2016-02-15 22:34:43 -0800

    Fix mosquitto install

--- installation/install_mosquitto.roborio.sh
+++ installation/install_mosquitto.roborio.sh
@@ -4,8 +4,8 @@ cd ~/
 
 # copy installation files into their expected places
 echo "[mosquitto] Copy files..."
-mkdir -p /usr/local/arm-frc-linux-gnueabi/lib/
-cp libzlib.so libwebsockets.so /usr/local/arm-frc-linux-gnueabi/lib/
+mkdir -p /usr/local/lib/
+cp libzlib.so libwebsockets.so /usr/local/lib/
 
 mkdir -p /usr/local/bin/
 cp mosquitto /usr/local/bin/
@@ -19,5 +19,7 @@ echo "[mosquitto] Configure init..."
 # Create the mosquitto RC info
 cd /etc/init.d/
 update-rc.d mosquitto.init.sh defaults
+
+ldconfig
 
 echo "[mosquitto] Finished"
