413b5da2a590b2bbd00134e12ae5f0618ab20a6b
Tom Bottiglieri <tom.bottiglieri@gmail.com>
2016-03-10 12:12:08 -0800

    auto: drop arm while driving over defenses

---
+++ src/com/team254/frc2016/auto/actions/SetArmModeAction.java
@@ -0,0 +1,36 @@
+package com.team254.frc2016.auto.actions;
+
+import com.team254.frc2016.subsystems.UtilityArm;
+
+/**
+ * Moves the arm to any mode then completes
+ */
+public class SetArmModeAction implements Action {
+
+    private UtilityArm.WantedState mDesiredState;
+    public SetArmModeAction(UtilityArm.WantedState state) {
+        mDesiredState = state;
+    }
+
+    private final UtilityArm mUtilityArm = UtilityArm.getInstance();
+    private boolean mIsDone = false;
+
+    @Override
+    public boolean isFinished() {
+        return mIsDone;
+    }
+
+    @Override
+    public void update() {
+    }
+
+    @Override
+    public void done() {
+    }
+
+    @Override
+    public void start() {
+        mUtilityArm.setWantedState(mDesiredState);
+        mIsDone = true;
+    }
+}
--- src/com/team254/frc2016/auto/modes/StayHighOneBall.java
+++ src/com/team254/frc2016/auto/modes/StayHighOneBall.java
@@ -5,6 +5,7 @@ import com.team254.frc2016.auto.AutoModeEndedException;
 import com.team254.frc2016.auto.actions.*;
 import com.team254.frc2016.subsystems.Drive;
 import com.team254.frc2016.subsystems.Shooter;
+import com.team254.frc2016.subsystems.UtilityArm;
 
 import java.util.Arrays;
 
@@ -18,6 +19,8 @@ public class StayHighOneBall extends AutoModeBase {
     private final boolean mIsBadBall;
     private final boolean mShouldDriveBack;
 
+    public final double DISTANCE_TO_DROP_ARM = 35;
+
     public StayHighOneBall(boolean isBadBall, boolean shouldDriveBack) {
         mIsBadBall = isBadBall;
         mShouldDriveBack = shouldDriveBack;
@@ -27,10 +30,15 @@ public class StayHighOneBall extends AutoModeBase {
     protected void routine() throws AutoModeEndedException {
         mShooter.setIsBadBall(mIsBadBall);
         runAction(new ParallelAction(Arrays.asList(DriveThenAimAction.makeForCommonConsts(),
-                new SeriesAction(Arrays.asList(new WaitForDistanceAction(125), new ArmToDriveModeAction())))));
+                new SeriesAction(Arrays.asList(
+                        new WaitForDistanceAction(DISTANCE_TO_DROP_ARM),
+                        new SetArmModeAction(UtilityArm.WantedState.PORTCULLIS)
+                ))
+        )));
         runAction(new WaitAction(1));
         runAction(new ShootWhenReadyAction());
         runAction(new WaitAction(0.75));
+        runAction(new ArmToDriveModeAction());
         if (mShouldDriveBack) {
             runAction(new DriveStraightAction(
                     -(mDrive.getLeftDistanceInches() + mDrive.getRightDistanceInches()) / 2.0 + 16.0, -45));
